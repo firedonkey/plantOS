87fe65ff9874fa9292fc60342e2b6c586728d994
Gary <lvganglvgang@gmail.com>
2026-07-22 18:39:01 -0700

    Bump master firmware to 0.1.10

--- device/esp32/include/firmware_version.h
+++ device/esp32/include/firmware_version.h
@@ -2,8 +2,8 @@
 
 namespace plantlab {
 
-constexpr char kMasterSoftwareVersion[] = "0.1.9";
-constexpr int kMasterSoftwareVersionCode = 1009;
+constexpr char kMasterSoftwareVersion[] = "0.1.10";
+constexpr int kMasterSoftwareVersionCode = 1010;
 constexpr char kCameraSoftwareVersion[] = "0.1.8";
 constexpr int kCameraSoftwareVersionCode = 1008;
 
